ac99ed7dd70f062daa1ddfaaad1df0c00ca9afb2
ahsanjaved-dev <ahsanjavedyousafzai555@gmail.com>
2025-12-16 23:01:27 +0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="flex gap-4 justify-center mt-8">
           <Link href="/login">
             <Button size="lg" variant="outline">
-              Sign In
+              Sign In Now
             </Button>
           </Link>
         </div>
